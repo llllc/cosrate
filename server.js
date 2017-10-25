96ec1eec09e4b417dd6bae3f58ed91d78a4eb764
James Li <james.li@opower.com>
2017-10-24 21:56:56 -0700

    Adding welcome message and guest counter

--- server.js
+++ server.js
@@ -14,10 +14,15 @@ channel.subscriptions = {};
 
 // Add a listener for the join event
 channel.on('join', function(id, client) {
+	// Setup welcome messages with current guest count
+	const welcome = `
+	Welcome! Guests online: ${this.listeners('broadcast').length+1}
+	`;
+	client.write(`${welcome}\n`);
 	this.clients[id] = client;
 	this.subscriptions[id] = (senderId, message) => {
 		if (id != senderId) {
-			this.clients[id].write(message);
+			this.clients[id].write(senderId + ': ' + message);
 		}
 	};
 	this.on('broadcast', this.subscriptions[id]);
@@ -38,10 +43,15 @@ channel.on('shutdown', () => {
 const server = net.createServer(client => {
 	const id = `${client.remoteAddress}:${client.remotePort}`;
 	channel.emit('join', id, client);
+
 	client.on('data', data => {
 		data = data.toString();
+		if (data === 'shutdown\r\n') {
+			channel.emit('shutdown\r\n');
+		}
 		channel.emit('broadcast', id, data);
 	});
+
 	client.on('close', () => {
 		channel.emit('leave', id);
 	});
